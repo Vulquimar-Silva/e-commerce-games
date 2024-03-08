a819a703228d43d0a409d416ef24775f8aafd6fe
Vulquimar Silva <vuquimar@hotmail.com>
2024-03-08 10:49:45 -0300

    config jest-styled-components

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -8,9 +8,19 @@ describe('<Main />', () => {
     const { container } = render(<Main />)
 
     // busca o elemento e verifica a existência dele
-    expect(screen.getByRole('heading', { name: /react avançado/i})).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
 
     // gerar snapshot
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    // renderiza o component
+    const { container } = render(<Main />)
+
+    // verifica se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
